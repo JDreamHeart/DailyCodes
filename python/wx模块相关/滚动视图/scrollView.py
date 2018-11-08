63a61246dc3341df4a050618fed10efc1563d757
JinZhang <2636423765@qq.com>
2018-11-08 18:27:07 +0800

    modify: 修改滚动视图实现逻辑
    详细信息:
    
    # 提交类型字段
    # [feat] 新功能（feature）
    # [fix] 修补bug
    # [modify] 修改或更新逻辑
    # [docs] 文档（documentation）
    # [refactor] 重构（即不是新增功能，也不是修改bug的代码变动）
    # [test] 增加测试

--- python/wx模块相关/滚动视图/scrollView.py
+++ python/wx模块相关/滚动视图/scrollView.py
@@ -2,9 +2,49 @@
 # @Author: JinZhang
 # @Date:   2018-11-08 17:00:50
 # @Last Modified by:   JinZhang
-# @Last Modified time: 2018-11-08 17:25:24
+# @Last Modified time: 2018-11-08 18:26:30
 
 import wx
+
+class ScrollView(wx.ScrolledWindow):
+	"""docstring for ScrollView"""
+	def __init__(self, parent, id = -1, params = {}, contentView = None):
+		self.initParams(params);
+		super(ScrollView, self).__init__(parent, id, size = self.params_["size"]);
+		self.setContentView(contentView);
+		
+	def initParams(self, params):
+		# 初始化参数
+		self.params_ = {
+			"size" : (0,0),
+		};
+		for k,v in params.items():
+			self.params_[k] = v;
+
+	def setContentView(self, contentView):
+		if contentView:
+			# 销毁原有内容节点
+			if hasattr(self, "contentView"):
+				self.contentView.Destroy();
+			# 重置父节点
+			if contentView.Parent != self:
+				contentView.Reparent(self);
+			# 重置self.contentView
+			self.contentView = contentView;
+			# 调整滚动条
+			self.adjustScrollbars();
+			# 初始化事件
+			self.initContentViewEvents();
+
+	def adjustScrollbars(self, event = None):
+		contentSize = self.contentView.GetSize();
+		print(contentSize[0], contentSize[1])
+		self.SetScrollbars(1, 1, contentSize[0], contentSize[1]);
+
+	def initContentViewEvents(self):
+		if hasattr(self, "contentView"):
+			self.contentView.Bind(wx.EVT_SIZE, self.adjustScrollbars);
+
 
 class MyFrame(wx.Frame):
 	def __init__(self, parent, id, title):
@@ -13,13 +53,14 @@ class MyFrame(wx.Frame):
 		panel = wx.Panel(self, size = (400,400))
 		panel.SetBackgroundColour("black")
 
-		self.scroller = wx.ScrolledWindow(panel, -1, size = (300,300))
-		self.scroller.SetBackgroundColour("red")
-		self.scroller.SetScrollbars(0, 0, 200, 900)
-
-		pnl = wx.Panel(self.scroller, pos = (100,100))
+		pnl = wx.Panel(self, pos = (100,100), size = (100,100))
 		pnl.SetBackgroundColour("green")
 		pnl.Bind(wx.EVT_LEFT_DOWN, self.onLeftDown)
+		self.pnl = pnl
+
+		self.scroller = ScrollView(panel, params = {"size" : (300,300)}, contentView = pnl)
+
+
 		self.SetMinSize((500,640))
 		box = wx.BoxSizer(wx.VERTICAL)
 		box.Add(panel, wx.EXPAND)
@@ -27,8 +68,7 @@ class MyFrame(wx.Frame):
 
 
 	def onLeftDown(self, event):
-		# print("0000000000000")
-		self.scroller.SetScrollbars(1, 1, 400, 900)
+		self.pnl.SetSize((400,600))
 
 if __name__ == '__main__':
 	app = wx.App(redirect=False)
