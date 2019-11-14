21e18ec022a6594dbf37e380775d09f4991935c8
JinZhang <2636423765@qq.com>
2019-11-14 18:45:31 +0800

    feat:新增文章内容外键表

--- python/django_learning/text_editor/Article/models.py
+++ python/django_learning/text_editor/Article/models.py
@@ -5,24 +5,24 @@ from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
-class Article(models.Model):
-    title = models.CharField(max_length=50, verbose_name='标题')
-    sub_title = models.CharField(max_length=50, blank=True, null=True, verbose_name='子标题')
-    # body = RichTextField()
-    brief = models.CharField(max_length=255)
-    content = RichTextUploadingField(verbose_name='正文')
-
-class ArticleContent(models.Model):
-    content = RichTextUploadingField(verbose_name='正文')
-
-    
 # class Article(models.Model):
 #     title = models.CharField(max_length=50, verbose_name='标题')
 #     sub_title = models.CharField(max_length=50, blank=True, null=True, verbose_name='子标题')
 #     # body = RichTextField()
 #     brief = models.CharField(max_length=255)
-#     cid = models.ForeignKey('ArticleContent', models.DO_NOTHING, db_column='cid')
+#     content = RichTextUploadingField(verbose_name='正文')
 
 # class ArticleContent(models.Model):
-#     id = models.IntegerField(primary_key=True)
 #     content = RichTextUploadingField(verbose_name='正文')
+
+    
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='标题')
+    sub_title = models.CharField(max_length=50, blank=True, null=True, verbose_name='子标题')
+    # body = RichTextField()
+    brief = models.CharField(max_length=255)
+    cid = models.ForeignKey('ArticleContent', models.DO_NOTHING, db_column='cid')
+
+class ArticleContent(models.Model):
+    id = models.IntegerField(primary_key=True)
+    content = RichTextUploadingField(verbose_name='正文')
--- python/django_learning/text_editor/Article/views.py
+++ python/django_learning/text_editor/Article/views.py
@@ -12,24 +12,32 @@ from Article import models;
 class ArticleForm(ModelForm):
     class Meta:
         model = models.Article
-        fields = ["title", "sub_title", "brief", "content"]
+        fields = ["title", "sub_title", "brief"]
 
     def __init__(self, *args, **kwargs):
         super(ArticleForm, self).__init__(*args, **kwargs);
         self.fields["brief"].widget = HiddenInput();
+
+# 文章表单
+class ArticleContentForm(ModelForm):
+    class Meta:
+        model = models.ArticleContent
+        fields = ["content"]
 
 @csrf_exempt
 def release(request):
     print("release get :", request.GET, "release post :", request.POST, "release files :", request.FILES);
     if "title" in request.POST:
         af = ArticleForm(request.POST);
-        print("==========af===========", af.is_valid(), af.cleaned_data);
-        if af.is_valid():
-            a = models.Article(title = af.cleaned_data["title"], sub_title = af.cleaned_data["sub_title"] or "", brief = af.cleaned_data["brief"], content = af.cleaned_data["content"]);
+        acf = ArticleContentForm(request.POST);
+        if af.is_valid() and acf.is_valid():
+            a = models.Article(title = af.cleaned_data["title"], sub_title = af.cleaned_data["sub_title"] or "", brief = af.cleaned_data["brief"], cid = models.ArticleContent(content = acf.cleaned_data["content"]));
             return render(request, "article_item.html", {
                 "article" : a,
                 "form" : af,
+                "content_form" : acf,
             });
     return render(request, "article.html", {
         "form" : ArticleForm(),
+        "content_form" : ArticleContentForm(),
     });
