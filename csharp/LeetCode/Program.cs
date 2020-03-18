39782887c02126cc1b2361aaa2e21fcc4ea53f01
JinZhang <15602291936@163.com>
2020-03-18 18:13:17 +0800

    refactor:更新中位数算法题解

--- csharp/LeetCode/Program.cs
+++ csharp/LeetCode/Program.cs
@@ -8,17 +8,18 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Solution6 s = new Solution6();
+            Solution7 s = new Solution7();
             DateTime startTime = DateTime.Now;
-            IList<IList<int>> ret = s.ThreeSum(new int[]{-2, 0, 0, 2, 2});
+            double ret = s.FindMedianSortedArrays(new int[]{}, new int[]{4, 5, 6, 8, 9});
             TimeSpan diffTime = DateTime.Now - startTime;
             Console.WriteLine("Result: [{0}];\nCoast Time: [{1}].", ret, diffTime.Milliseconds);
-            StringBuilder str = new StringBuilder();
-            for (int i = 0; i < ret.Count; i ++) {
-                str.AppendJoin(',', ret[i]);
-                str.Append("|");
-            }
-            Console.WriteLine("Result: [{0}].", str.ToString());
+            // StringBuilder str = new StringBuilder();
+            // for (int i = 0; i < ret.Count; i ++) {
+            //     str.AppendJoin(',', ret[i]);
+            //     str.Append("|");
+            // }
+            // Console.WriteLine("Result: [{0}].", str.ToString());
+            Console.WriteLine("Result: {0}", ret);
             // Console.ReadLine();
         }
     }
--- csharp/LeetCode/Solution7.cs
+++ csharp/LeetCode/Solution7.cs
@@ -21,12 +21,98 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 
 namespace LeetCode {
     public class Solution7 {
+        // @return diff count
+        void checkArrayMedian(int[] nums, int h, int r, int diff, out int hIdx, out int rIdx) {
+            int median = (int)diff/2;
+            hIdx = h + median;
+            rIdx = r - median;
+        }
         public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-            return 0;
+            int h1 = 0, h2 = 0, r1 = nums1.Length - 1, r2 = nums2.Length - 1;
+            while(h1 <= r1 && h2 <= r2) {
+                Console.WriteLine("h1: {0}, r1: {1}; h2: {2}, r2: {3}", h1, r1, h2, r2);
+                // 判断剩余数
+                if (h1 == r1 || h2 == r2) {
+                    if (h1 == r1 && h2 == r2) {
+                        return (double) (nums1[h1] + nums2[h2]) / 2;
+                    } else if (h1 == r1) {
+                        if (nums1[h1] > nums2[h2] && nums1[h1] < nums2[r2]) {
+                            h2++;
+                            r2--;
+                        } else if (nums1[h1] <= nums2[h2]) {
+                            h1++;
+                            r2--;
+                        } else {
+                            h1++;
+                            h2++;
+                        }
+                    } else if (h2 == r2) {
+                        if (nums2[h2] > nums1[h1] && nums2[h2] < nums1[r1]) {
+                            h1++;
+                            r1--;
+                        } else if (nums2[h2] <= nums1[h1]) {
+                            h2++;
+                            r1--;
+                        } else {
+                            h2++;
+                            h1++;
+                        }
+                    }
+                    continue;
+                }
+                // 获取移除间隔
+                int diff = r1 - h1 - 1;
+                if (diff > r2 - h2 - 1) {
+                    diff = r2 - h2 - 1;
+                }
+                // 获取中位数
+                int hIdx1 = 0, rIdx1 = 0;
+                checkArrayMedian(nums1, h1, r1, diff, out hIdx1, out rIdx1);
+                int hIdx2 = 0, rIdx2 = 0;
+                checkArrayMedian(nums2, h2, r2, diff, out hIdx2, out rIdx2);
+                Console.WriteLine("---hIdx1: {0}, rIdx1: {1}; hIdx2: {2}, rIdx2: {3};---", hIdx1, rIdx1, hIdx2, rIdx2);
+                // 去掉头部
+                if (nums1[hIdx1] < nums2[hIdx2]) {
+                    h1 = hIdx1 + 1;
+                } else {
+                    h2 = hIdx2 + 1;
+                }
+                // 去掉尾部
+                if (nums1[rIdx1] > nums2[rIdx2]) {
+                    r1 = rIdx1 - 1;
+                } else {
+                    r2 = rIdx2 - 1;
+                }
+            }
+            // 返回结果
+            if (h1 == r1 && h2 == r2) {
+                return (double) (nums1[h1] + nums2[h2]) / 2;
+            } else if (h1 == r1) {
+                return (double) nums1[h1];
+            } else if (h2 == r2) {
+                return (double) nums2[h2];
+            }
+            // 获取中位数
+            int h = h1, r = r1;
+            int[] nums = nums1;
+            if (h1 > r1) {
+                h = h2;
+                r = r2;
+                nums = nums2;
+            }
+            int hIdx = 0, rIdx = 0;
+            checkArrayMedian(nums, h, r, r - h - 1, out hIdx, out rIdx);
+            StringBuilder str = new StringBuilder();
+            Console.WriteLine("nums: [{0}], h: {1}, r: {2}, hIdx: {3}, rIdx: {4}", str.AppendJoin(',', nums), h, r, hIdx, rIdx);
+            if (rIdx - hIdx == 2) {
+                return nums[hIdx+1];
+            }
+            return (double) (nums[hIdx] + nums[rIdx]) / 2;
         }
     }
 }
